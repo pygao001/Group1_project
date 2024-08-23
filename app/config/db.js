5fdd8e47501931bb2230131c6f61b73363cef5a4
thisIsRealGary <952361257@qq.com>
2024-08-23 14:43:46 +0800

    adding stocks.xlsx

--- app/config/db.js
+++ app/config/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: 'root',
     database: 'music_db',
 });
 
